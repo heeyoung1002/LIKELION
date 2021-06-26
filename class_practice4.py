3fae8fc4342ccae2d42be9079255819878e98ca8
heeyoung1002 <lusy1223@gmail.com>
2021-06-27 00:19:45 +0900

    WEEK 3 - PRACTICES

---
+++ class_practice4.py
@@ -0,0 +1,44 @@
+class Car:
+
+    def __init__(self, name, color, fuel):
+        self.name = name
+        self.color = color
+        self.fuel = fuel
+        self.mileage = 0
+
+
+    def accel(self):
+        self.fuel -= 2
+        self.mileage += 10
+
+
+class Ferrari(Car):
+    
+    def __init__(self):
+        super().__init__("Ferrari", "Rosso Corsa", 50)
+        self.slogan = "We are the competition"
+
+
+class Lamborghini(Car):
+
+    def __init__(self):
+        super().__init__("Lamborghini", "Giallo Spica", 40)
+        self.slogan = "We are not supercars. We are Lamborghini."
+
+ferrari = Ferrari()
+lamborghini = Lamborghini()
+
+print(ferrari)
+print(lamborghini)
+
+ferrari.accel()
+lamborghini.accel()
+
+ferrari = Ferrari()
+lamborghini = Lamborghini()
+
+print(ferrari)
+print(lamborghini)
+
+ferrari.accel()
+lamborghini.accel()
