dea5ae8e125d6cdf8ed5071094c2559b65527ca6
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-06-27 00:53:41 +0900

    WEEK 2 - Practices

--- list_basics.py
+++ list_basics.py
@@ -1,9 +1,13 @@
+#list 선언
 singers = ['김법수', '나얼', '박효신', '이수']
 
+#두번째 index 출력
 print(singers[2])
 
+#0이상 2미만까지 출력
 print(singers[0:2])
 
+#list의 길이 출력
 print(len(singers))
 
 print(len(singers[0:2]))
