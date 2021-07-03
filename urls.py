35c802ee02ddc421e658629f7a9d3026778b8fdb
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-03 12:48:29 +0900

    WEEK 4 - Practices
    project 의 urls.py에서 app의 urls로 (분리) 연결해주는 연습

--- urls.py
+++ urls.py
@@ -1,27 +1,11 @@
-"""mangostagram URL Configuration
+# post app 과 관련된 url 패턴을 post app 내부에서 독자적인 파일로 관리할 것인데
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-from posts import views
+from . import views  #현재 디렉토리 (.) 에있는 같은 선상에 있는 views를 가져올래
 
-# server가 serving 가능한 URL 이 나와있는 모듈이다
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/', views.index),
-    path('camilla/', views.camilla),
-    path('', views.landingpage),   #landing page등록을 위한 url 등록
+    path('', views.index),
+    path('20/', views.detail),
+    path('20/comments', views.comments)
 ]
--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    print('This is a post/ URL 패턴입니다') #로그로 남길수 있게 print
+    print('This is a post/ URL 패턴입니다') #로그로 남길수 있게 print, 
     return HttpResponse('Hello Camilla!')
 
 def camilla(request):
@@ -12,3 +12,9 @@ def camilla(request):
 
 def landingpage(request):
     return HttpResponse('This is a landing page')
+
+def detail(request):
+    return HttpResponse('This is a detail page')
+
+def comments(request):
+    return HttpResponse('This is a comments page')
