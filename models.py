fc6a08d2dee0f05fa0d8f7fddd59faa0b14b4135
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-07 23:49:25 +0900

    WEEK 5 - Practices

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#게시물을 저장할 클라스 (게시물과 관련된 데이터이다)
+#장고의 Model클래스를 상속받는 post라는 클래스
+class Post(models.Model):
+
+    #데이터베이스 테이블의 column이 된다 (열)
+    author = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.author}: {self.body}'
