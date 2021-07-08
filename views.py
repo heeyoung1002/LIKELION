1335af782e59f2f6901f86f5ff0d73086cfea5d4
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-08 23:16:50 +0900

    WEEK 5 - Practices

--- views.py
+++ views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+# Create your views here.\
 
-class Member: #Member 라는 클라스 형성
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        
 def index(request):
-    
-    context = { #딕셔너리를 생성해서, 채워진 데이터를 render함수에 3번째 인자로 대입을 해주는 것이다    
-        'post': { #post 라는 key는 딕셔너리 형태로 구성
-            'author': 'Camilla',
-            'body': 'My First Django Project'
-        },
-        'numbers': [10, 20, 30], #numbers의 key는 리스트로 구성
-        'member': Member('Camilla', 30) #member라는 key는 클라스로
+    #post table에 저장되어있는 모든 데이터를 posts 라는 변수에 저장한다
+    posts = Post.objects.all().order_by('created_at')
 
+    context = { # 게시물 딕셔너리를 여러개 담고 있는 리스트
+        'posts' : posts
     }
+
     return render(request, 'posts/index.html', context)
     # context라는 복합적인 데이터를 담고있는 데이터를 post/index.html이라는 template 에 전달해주면
     # index.html 파일에서는 context라는 딕셔너리에 있는 데이터를 
