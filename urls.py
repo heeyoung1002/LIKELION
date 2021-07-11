981477cf7f0f904ad8c5eebeb46efd719d808dee
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-11 22:56:04 +0900

    WEEK 5 - Practices

--- urls.py
+++ urls.py
@@ -1,11 +1,22 @@
-# post app 과 관련된 url 패턴을 post app 내부에서 독자적인 파일로 관리할 것인데
+"""mangostagram URL Configuration
 
-from django.urls import path
-from . import views  #현재 디렉토리 (.) 에있는 같은 선상에 있는 views를 가져올래
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include #include 함수를 추가로 import
 
 urlpatterns = [
-    path('', views.index),
-    path('20/', views.detail),
-    path('20/comments', views.comments)
+    path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls')), #posts/ 라는 pattern으로 요청이 들어오면, 여기서 직접 view함수를 지정해주는게 아니라 include 함수를 사용해서 post 패키지 내부에 있는 url모듈을 참고해                
 ]
