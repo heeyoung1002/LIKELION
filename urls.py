e181715a5b2f5e8898b2a708cc00ba5b38716eb0
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-19 23:50:43 +0900

    WEEK 5 - Project

--- urls.py
+++ urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.index_books, name='booksindex'),
-    path('videos/', views.index_videos, name='videosindex'),
-    path('websites/', views.index_websites, name='websitesindex'),
+    path('', views.index, name='index'),
 ]
--- views.py
+++ views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-def index_books(request):
-    return render (request, 'library/index.html')
-
-def index_videos(request):
-    return HttpResponse('This is Video page')
-
-def index_websites(request):
-    return HttpResponse('This is Website page')
+def index(request):
+    return render (request, 'login/index.html')
