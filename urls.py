258909ac1bb50b1eb509f18dbd20dc4790e35058
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-23 01:26:10 +0900

    WEEK 6 - Project

--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('books/', views.index_books, name='booksindex'),
     path('videos/', views.index_videos, name='videosindex'),
     path('websites/', views.index_websites, name='websitesindex'),
-]
+    path('books/<int:book_id>/', views.index_books_detail)
+] 
