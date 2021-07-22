1e0b6ae939316cdd588953b8409a94912cccd27e
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-22 23:43:23 +0900

    WEEK 6 - Project

--- models.py
+++ models.py
@@ -2,14 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-#게시물을 저장할 클라스 (게시물과 관련된 데이터이다)
-#장고의 Model클래스를 상속받는 post라는 클래스
-class Post(models.Model):
+class Book(models.Model):
 
-    #데이터베이스 테이블의 column이 된다 (열)
+    cover = models.ImageField (upload_to='',default='')
+    level = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    body = models.TextField()
-    created_at = models.DateTimeField()
+    urllink = models.URLField(max_length=200)
+    about = models.TextField(null=True, default='')
 
-    def __str__(self):
-        return f'{self.author}: {self.body}'
+    def __str__(self):  
+        return f'{self.level} : {self.title} : {self.author}'
+
+
+class Video(models.Model):
+
+    cover = models.ImageField (upload_to='',blank=True, null=True) #이부분 수정해야함
+    level = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
+    urllink = models.URLField(max_length=200)
+    about = models.TextField(null=True, default='')
+    how = models.TextField(null=True, default='')
+
+    def __str__(self): #해당 인스턴스가 어떤 attribute 가지고 있는지 간단하게 
+        return f'{self.level} : {self.title}'
+
+
+class Website(models.Model):
+
+    cover = models.ImageField (upload_to='',blank=True, null=True) #이부분 수정해야함
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    urllink = models.URLField(max_length=200)
+    about = models.TextField(null=True, default='')
+
+    def __str__(self): #해당 인스턴스가 어떤 attribute 가지고 있는지 간단하게 
+        return f'{self.category} : {self.title}'
--- views.py
+++ views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Video, Website
 
 
 # Create your views here.
 
 def index_books(request):
+    books = Book.objects.all() #Book 이라는 테이블에 저장되어있는 모든 파일을 불러와서 books라는 변수에 저장한다
+    
     context = {
-        'post' : {
-            'title': 'book title',
-            'image': 'book image',
-        }
+        'books' : books,
+        'levels' : ['AR 0.1 - 0.9대', 'AR 1.0 - 1.9대', 'AR 2.0 - 2.9대', 'AR 3.0 - 3.9대', 'AR 4.0 - 4.9대', 'AR 5.0 - 5.9대'],
     }
-    return render (request, 'library/index.html', context)
+    return render (request, 'library/index_book.html', context)
+
 
 def index_videos(request):
-    return render (request, 'library/index.html')
+    videos = Video.objects.all() #Video 이라는 테이블에 저장되어있는 모든 파일을 불러와서 videos라는 변수에 저장한다 
+
+    context = {
+        'videos' : videos,
+        'levels' : ['미취학 아동', '초등 저학년 추천영상', '초등 고학년 추천영상']
+    }
+    return render (request, 'library/index_video.html')
+
 
 def index_websites(request):
-    return render (request, 'library/index.html')
+
+    return render (request, 'library/index_library.html')
