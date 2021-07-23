d02baa164797f0150544de89e3fe1246b9ad252d
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-23 12:45:48 +0900

    WEEK 6 - Practices

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'library'
 urlpatterns = [
     path('books/', views.index_books, name='booksindex'),
+    path('books/<int:book_id>/', views.index_books_detail, name='bookdetail'),
     path('videos/', views.index_videos, name='videosindex'),
+    path('videos/<int:video_id>/', views.index_videos_detail, name='videodetail'),
     path('websites/', views.index_websites, name='websitesindex'),
-    path('books/<int:book_id>/', views.index_books_detail)
+    path('websites/<int:video_id>/', views.index_websites_detail, name='websitedetail'),
 ] 
