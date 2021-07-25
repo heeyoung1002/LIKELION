657c39f9ed60845e7bf69c6b251c118ebbe32fe9
heeyoung1002 <78261017+heeyoung1002@users.noreply.github.com>
2021-07-25 23:53:19 +0900

    WEEK 6 - Proejct

--- urls.py
+++ urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
 
-
-app_name = 'library'
+app_name = 'login'
 
 urlpatterns = [
-    path('books/', views.index_books, name='booksindex'),
-    path('books/<int:book_id>/', views.index_books_detail, name='bookdetail'),
-    path('videos/', views.index_videos, name='videosindex'),
-    path('videos/<int:video_id>/', views.index_videos_detail, name='videodetail'),
-    path('websites/', views.index_websites, name='websitesindex'),
-    path('websites/<int:website_id>/', views.index_websites_detail, name='websitedetail'),
-] 
+    path('', views.index, name='loginindex'),
+]
